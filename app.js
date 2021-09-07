c154cd27bc7a2ab828cb1ba97913e58f3dc88317
smccourtb <shawnmccourtb@gmail.com>
2021-09-07 13:44:03 -0400

    add rudimentary button to open a form for inputting books

--- app.js
+++ app.js
@@ -10,5 +10,28 @@ function Book(title, author, numPages, read) {
 
 const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
 
-console.log(book1.info())
-console.log(book1.prototype.title)
+
+let myLibrary = [];
+
+function addBookToLibrary() {
+    pass
+  }
+
+const addBookButton = document.getElementById("addBook")
+
+addBookButton.addEventListener('click', () => {
+    openForm();
+})
+
+// functions to show/hide form on button press
+function openForm() {
+    document.getElementById("addBookForm").style.display = "block";
+  }
+
+function closeForm() {
+    document.getElementById("addBookForm").style.display = "none";
+  }
+// 1. get input from user using addBookToLibrary function
+// 2. store that input as a BOOK object (using Object.create()?)
+// 3. function to shows all book in the library with the relative info.
+// loop through my library and display the info
