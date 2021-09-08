613c8ef3a9227c9c45e00737eaebfae56c08aabb
smccourtb <shawnmccourtb@gmail.com>
2021-09-08 11:42:22 -0400

    add clear form function to clear input fields when form is submitted

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ function Book(title, author, numPages, read) {
 };
 
 Book.prototype.info = function() {
-    if(this.read) {
-        this.read=true
-    };
     return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read}`
 };
 
@@ -18,7 +15,7 @@ function addBookToLibrary() {
     let title = document.getElementById("bookTitle").value
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
-    let read = document.getElementById("read").value
+    let read = document.getElementById("read").checked
     const book1 = new Book(title, author, pages, read)
     console.log(book1.info())
     myLibrary.push(book1)
@@ -34,6 +31,7 @@ addBookButton.addEventListener('click', () => {
 submitNewBook.addEventListener('click', () => {
     addBookToLibrary();
     closeForm();
+    clearForm();
 })
 
 // functions to show/hide form on button press
@@ -44,6 +42,17 @@ function openForm() {
 function closeForm() {
     document.getElementById("addBookForm").style.display = "none";
   }
+
+function clearForm() {
+    const inputs = document.querySelectorAll("input")
+    for(i of inputs){
+        i.value = "";
+        if(i.type === "checkbox") {
+            console.log(i);
+            i.checked = false;
+        };
+    };
+};
 // 1. get input from user using addBookToLibrary function
 // 2. store that input as a BOOK object (using Object.create()?)
 // 3. function to shows all book in the library with the relative info.
