3295d1e8c34687dd969e0e581895d52a0c480977
smccourtb <shawnmccourtb@gmail.com>
2021-09-08 14:40:30 -0400

    entries are listed in the table upon form submittion

--- app.js
+++ app.js
@@ -7,27 +7,30 @@ function Book(title, author, numPages, read) {
     this.read = read;
 };
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read}`
-};
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.read}`
+// };
 
 function addBookToLibrary() {
     let title = document.getElementById("bookTitle").value
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
     let read = document.getElementById("read").checked
+    
     const book1 = new Book(title, author, pages, read)
-    console.log(book1.info())
+    reloadLibrary(book1)
     myLibrary.push(book1)
+   // localStorage.setItem("library", [myLibrary])
   }
 
 const addBookButton = document.getElementById("addBook")
 const submitNewBook = document.getElementById("newBookSubmit")
 const addBookForm = document.getElementById("addBookForm")
-
+const library = document.querySelector("table")
 addBookButton.addEventListener('click', () => {
     openForm();
 })
+
 submitNewBook.addEventListener('click', () => {
     addBookToLibrary();
     closeForm();
@@ -52,6 +55,16 @@ function clearForm() {
             i.checked = false;
         };
     };
+};
+
+function reloadLibrary(book) {
+    const entry = document.createElement('tr')
+    for(let i in book) {
+        const data = document.createElement('td');
+        data.textContent = book[i]
+        entry.appendChild(data)
+    };
+    library.appendChild(entry)
 };
 // 1. get input from user using addBookToLibrary function
 // 2. store that input as a BOOK object (using Object.create()?)
